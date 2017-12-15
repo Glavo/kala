669086eb5bad800408d3cf6803c32d9c23db34bd
Glavo <zjx001202@126.com>
2017-12-15 23:59:43 +0800

    auto commit

--- src/main/kotlin/org/glavo/kala/control/Either.kt
+++ src/main/kotlin/org/glavo/kala/control/Either.kt
@@ -1,65 +1,35 @@
 package org.glavo.kala.control
 
+import org.glavo.kala.collection.Iterators
+
 sealed class Either<out L, out R> : Iterable<R> {
-    data class LeftProjection<out L, out R>(val either: Either<L, R>) {}
-    data class RightProjection<out L, out R>(val either: Either<L, R>) {}
+    open val leftValue: L
+        get() = throw NoSuchElementException("leftValue on Right")
 
-    abstract fun isLeft(): Boolean
-
-    abstract fun isRight(): Boolean
-
-    fun left(): LeftProjection<L, R> = LeftProjection(this)
-
-    fun right(): RightProjection<L, R> = RightProjection(this)
-
-    abstract val leftValue: L
-
-    abstract val rightValue: R
-
-    inline fun forEach(f: (R) -> Unit): Unit {
-        if(isRight())
-            f(rightValue)
-    }
-
-    inline fun <T> map(mapper: (R) -> T): Either<L, T> {
-        @Suppress("UNCHECKED_CAST")
-        return if(isLeft()) this as Either<L, T> else Right(mapper(rightValue))
-    }
-
-    operator override fun iterator(): Iterator<R> = object : Iterator<R> {
-        var end = isLeft()
-
-        override fun hasNext(): Boolean {
-            return !end
-        }
-
-        override fun next(): R {
-            return if(end) throw NoSuchElementException() else rightValue
-        }
-
-    }
+    open val rightValue: R
+        get() = throw NoSuchElementException("rightValue on Left")
 }
 
 data class Left<out T>(val value: T) : Either<T, Nothing>() {
-    override fun isLeft(): Boolean = true
-
-    override fun isRight(): Boolean = false
-
     override val leftValue: T
         get() = value
 
-    override val rightValue: Nothing
-        get() = throw NoSuchElementException("Left.rightValue")
+    override fun iterator(): Iterator<Nothing> = Iterators.emptyIterator
 }
 
 data class Right<out T>(val value: T) : Either<Nothing, T>() {
-    override fun isLeft(): Boolean = false
-
-    override fun isRight(): Boolean = true
-
-    override val leftValue: Nothing
-        get() = throw NoSuchElementException("")
-
     override val rightValue: T
         get() = value
+
+    override fun iterator(): Iterator<T> = object : Iterator<T> {
+        private var flag = false
+
+        override fun hasNext(): Boolean = flag
+
+        override fun next(): T {
+            flag = true
+            return value
+        }
+
+    }
 }
